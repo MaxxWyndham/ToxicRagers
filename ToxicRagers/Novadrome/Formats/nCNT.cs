4c91c9e3c5eebe3fa050a6ef1ec368b08a424320
Errol Errolson <errol@toxic-ragers.co.uk>
2014-04-11 16:20:26 +0100

    Novadrome support added.  CNT, MTL and XT2

---
+++ ToxicRagers/Novadrome/Formats/nCNT.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using ToxicRagers.Helpers;
+
+namespace ToxicRagers.Novadrome.Formats
+{
+    public class CNT
+    {
+        CNT parent;
+        string name;
+        string nodeName;
+        string modelName;
+        string section;
+        Matrix3D transform;
+        List<CNT> childNodes = new List<CNT>();
+
+        public string Name { get { return name; } }
+        public string Model { get { return modelName; } }
+        public string Section { get { return section; } }
+        public Matrix3D Transform { get { return transform; } }
+
+        public CNT Parent { get { return parent; } }
+        public List<CNT> Children { get { return childNodes; } }
+
+        public Matrix3D CombinedTransform
+        {
+            get
+            {
+                var m = transform;
+                var cnt = this;
+
+                while (cnt.parent != null)
+                {
+                    cnt = cnt.parent;
+
+                    m *= cnt.transform;
+                }
+
+                return m;
+            }
+        }
+
+        public static CNT Load(string Path)
+        {
+            FileInfo fi = new FileInfo(Path);
+            Logger.LogToFile("{0}", Path);
+            CNT cnt;
+
+            using (BinaryReader br = new BinaryReader(fi.OpenRead()))
+            {
+                if (br.ReadByte() != 69 ||
+                    br.ReadByte() != 35 ||
+                    br.ReadByte() != 0 ||
+                    br.ReadByte() != 4)
+                {
+                    Logger.LogToFile("{0} isn't a valid CNT file", Path);
+                    return null;
+                }
+
+                cnt = Load(br);
+
+                Logger.LogToFile("{0} :: {1}", br.BaseStream.Position.ToString("X"), br.BaseStream.Length.ToString("X"));
+                if (br.BaseStream.Position != br.BaseStream.Length) { Logger.LogToFile("Still has data remaining"); }
+            }
+
+            return cnt;
+        }
+
+        private static CNT Load(BinaryReader br, CNT parent = null)
+        {
+            // The Load(BinaryReader) version skips the header check and is used for recursive loading
+            CNT cnt = new CNT();
+            if (parent != null) { cnt.parent = parent; }
+
+            Logger.LogToFile("{0}", br.BaseStream.Position.ToString("X"));
+
+            int nameLength = (int)br.ReadUInt32();
+            int padding = (((nameLength / 4) + (nameLength % 4 > 0 ? 1 : 0)) * 4) - nameLength;
+
+            cnt.name = br.ReadString(nameLength);
+            br.ReadBytes(padding);
+
+            Logger.LogToFile("Name: \"{0}\" of length {1}, padding of {2}", cnt.Name, nameLength, padding);
+
+            byte flags = br.ReadByte();
+            if (flags != 0)
+            {
+                Logger.LogToFile("Flags: {0}", flags);
+                br.ReadByte();
+            }
+
+            Logger.LogToFile("This is usually 0: {0}", br.ReadSingle());
+
+            cnt.transform = new Matrix3D(
+                                br.ReadSingle(), br.ReadSingle(), br.ReadSingle(),
+                                br.ReadSingle(), br.ReadSingle(), br.ReadSingle(),
+                                br.ReadSingle(), br.ReadSingle(), br.ReadSingle(),
+                                br.ReadSingle(), br.ReadSingle(), br.ReadSingle()
+                            );
+
+            cnt.section = br.ReadString(4);
+            switch (cnt.section)
+            {
+                case "EMIT":
+                    br.ReadBytes(177);
+                    break;
+
+                case "MODL":
+                    nameLength = (int)br.ReadUInt32();
+                    padding = (((nameLength / 4) + (nameLength % 4 > 0 ? 1 : 0)) * 4) - nameLength;
+
+                    cnt.modelName = br.ReadString(nameLength);
+                    br.ReadBytes(padding);
+
+                    Logger.LogToFile("{0}: \"{1}\" of length {2}, padding of {3}", cnt.section, cnt.modelName, nameLength, padding);
+                    break;
+
+                case "NULL":
+                    break;
+
+                default:
+                    throw new NotImplementedException(string.Format("Load code for CNT section {0} does not exist! ({1})", cnt.Section, br.BaseStream.Position.ToString("X")));
+            }
+
+            int childNodes = (int)br.ReadUInt32();
+
+            for (int i = 0; i < childNodes; i++)
+            {
+                Logger.LogToFile("Loading child {0} of {1}", (i + 1), childNodes);
+                cnt.childNodes.Add(Load(br, cnt));
+            }
+
+            br.ReadUInt32();    // Terminator
+
+            return cnt;
+        }
+
+        public void Save(string Path)
+        {
+            using (BinaryWriter bw = new BinaryWriter(new FileStream(Path, FileMode.Create)))
+            {
+                bw.Write(new byte[] { 69, 35, 0, 4 });
+
+                Save(bw, this);
+            }
+        }
+
+        private static void Save(BinaryWriter bw, CNT cnt)
+        {
+            int nameLength = cnt.Name.Length;
+            int padding = (((nameLength / 4) + (nameLength % 4 > 0 ? 1 : 0)) * 4) - nameLength;
+
+            bw.Write(nameLength);
+            bw.WriteString(cnt.Name);
+            bw.Write(new byte[padding]);
+
+            bw.Write((byte)0);
+
+            bw.Write((int)0);
+
+            bw.Write(cnt.Transform.M11);
+            bw.Write(cnt.Transform.M12);
+            bw.Write(cnt.Transform.M13);
+            bw.Write(cnt.Transform.M21);
+            bw.Write(cnt.Transform.M22);
+            bw.Write(cnt.Transform.M23);
+            bw.Write(cnt.Transform.M31);
+            bw.Write(cnt.Transform.M32);
+            bw.Write(cnt.Transform.M33);
+            bw.Write(cnt.Transform.M41);
+            bw.Write(cnt.Transform.M42);
+            bw.Write(cnt.Transform.M43);
+
+            bw.WriteString(cnt.Section);
+
+            switch (cnt.Section)
+            {
+                case "MODL":
+                    nameLength = cnt.Model.Length;
+                    padding = (((nameLength / 4) + (nameLength % 4 > 0 ? 1 : 0)) * 4) - nameLength;
+
+                    bw.Write(nameLength);
+                    bw.WriteString(cnt.Name);
+                    bw.Write(new byte[padding]);
+                    break;
+
+                case "NULL":
+                    break;
+
+                default:
+                    throw new NotImplementedException(string.Format("Save code for CNT section {0} does not exist!", cnt.Section));
+            }
+
+            bw.Write(cnt.Children.Count);
+            foreach (CNT c in cnt.Children) { Save(bw, c); }
+            bw.Write((int)0);
+        }
+
+        // This seems wrong.  I am very tired.
+        public CNT FindByName(string name)
+        {
+            CNT match = null;
+
+            if (this.name == name)
+            {
+                match = this;
+            }
+            else
+            {
+                foreach (CNT c in this.childNodes)
+                {
+                    match = c.FindByName(name);
+                    if (match != null) { break; }
+                }
+            }
+
+            return match;
+        }
+    }
+}
---
+++ ToxicRagers/Novadrome/Formats/nXT2.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using ToxicRagers.Helpers;
+
+namespace ToxicRagers.Novadrome.Formats
+{
+    public class XT2
+    {
+        string name;
+        bool bIndexed;
+        int dataSize;
+        int width;
+        int height;
+        D3DBaseTexture header;
+        string textureName;
+        public List<MipMap> mipMaps = new List<MipMap>();
+
+        public string Name { get { return name; } }
+        public string Texture { get { return textureName; } }
+
+        public string Format
+        {
+            get
+            {
+                switch (header.DataFormat)
+                {
+                    case D3DFormat.A8R8G8B8:
+                        return "8888";
+
+                    case D3DFormat.DXT1:
+                        return "DXT1";
+
+                    case D3DFormat.DXT2:
+                        return "DXT3";
+                }
+
+                return null;
+            }
+        }
+
+        public static XT2 Load(string Path)
+        {
+            FileInfo fi = new FileInfo(Path);
+            Console.WriteLine("{0}", Path);
+            XT2 xt2 = new XT2();
+
+            xt2.name = fi.Name.Replace(fi.Extension, "");
+
+            using (BEBinaryReader br = new BEBinaryReader(fi.OpenRead()))
+            {
+                Console.WriteLine("Always 0 : {0}", br.ReadUInt32());
+
+                int magic = (int)br.ReadUInt32();
+
+                xt2.dataSize = (int)br.ReadUInt32();
+                Console.WriteLine("Always 52 : {0}", br.ReadUInt32());
+                Console.WriteLine("Always 0 : {0}", br.ReadUInt16());
+                xt2.width = br.ReadUInt16();
+                xt2.height = br.ReadUInt16();
+                Console.WriteLine("{0} {1}", br.ReadUInt16(), br.ReadUInt16());
+
+                xt2.header = new D3DBaseTexture(br);
+
+                Console.WriteLine("{0}x{1} :: {2} :: {3} :: {4} :: {5}", xt2.width, xt2.height, xt2.header.DataFormat, xt2.header.Endian, xt2.dataSize, magic);
+
+                int W = 0;
+                int H = 0;
+                int TexelPitch = 0;
+                int DataSize = 0;
+                int OutSize = 0;
+                int w = xt2.width;
+                int h = xt2.height;
+
+                switch (xt2.header.DataFormat)
+                {
+                    case D3DFormat.A8R8G8B8:
+                        W = w;
+                        H = h;
+                        TexelPitch = 4;
+                        DataSize = w * h * 4;
+                        break;
+
+                    case D3DFormat.DXT1:
+                        W = w / 4;
+                        H = h / 4;
+                        TexelPitch = 8;
+                        DataSize = w * h / 2;
+                        break;
+
+                    case D3DFormat.DXT2:
+                        W = w / 4;
+                        H = h / 4;
+                        TexelPitch = 16;
+                        DataSize = W * H;
+                        break;
+                }
+
+                if (H % 128 != 0) { H = H + (128 - H % 128); }
+
+                switch (xt2.header.DataFormat)
+                {
+                    case D3DFormat.A8R8G8B8:
+                        OutSize = W * H * TexelPitch;
+                        break;
+
+                    case D3DFormat.DXT1: 
+                        OutSize = W * H * TexelPitch;
+                        break;
+
+                    case D3DFormat.DXT2:
+                        DataSize = w * H * 2;
+                        OutSize = W * H * TexelPitch;
+                        break;
+                }
+
+                byte[] data = new byte[DataSize];
+                byte[] outdata = new byte[OutSize];
+
+                Array.Copy(br.ReadBytes(DataSize), data, DataSize);
+
+                int step = (xt2.header.Endian == 1 ? 2 : 4);
+                for (int i = 0; i < data.Length; i += step)
+                {
+                    for (int j = 0; j < step / 2; j++)
+                    {
+                        byte tmp = data[i + j];
+                        data[i + j] = data[i + step - j - 1];
+                        data[i + step - j - 1] = tmp;
+                    }
+                }
+
+                for (int y = 0; y < H; y++)
+                {
+                    for (int x = 0; x < W; x++)
+                    {
+                        int offset = Xbox360ConvertTextureAddress(x, y, W, TexelPitch);
+                        if (offset * TexelPitch < data.Length) { Array.Copy(data, offset * TexelPitch, outdata, (x + y * W) * TexelPitch, TexelPitch); }
+                    }
+                }
+
+                var mip = new MipMap();
+                mip.Width = xt2.width;
+                mip.Height = xt2.height;
+                mip.Data = data;
+                xt2.mipMaps.Add(mip);
+            }
+
+            return xt2;
+        }
+
+        public static int Xbox360ConvertTextureAddress(int x, int y, int w, int TexelPitch)
+        {
+            int alignedWidth;
+            int logBpp;
+            int Macro;
+            int Micro;
+            int Offset;
+
+            alignedWidth = (w + 31) &~ 31;
+            logBpp = (TexelPitch >> 2) + ((TexelPitch >> 1) >> (TexelPitch >> 2));
+            Macro = ((x >> 5) + (y >> 5) * (alignedWidth >> 5)) << (logBpp + 7);
+            Micro = (((x & 7) + ((y & 6) << 2)) << logBpp);
+            Offset = Macro + ((Micro &~ 15) << 1) + (Micro & 15) + ((y & 8) << (3 + logBpp)) + ((y & 1) << 4);
+            return (((Offset &~ 511) << 3) + ((Offset & 448) << 2) + (Offset & 63) + ((y & 16) << 7) + (((((y & 8) >> 2) + (x >> 3)) & 3) << 6)) >> logBpp;
+        }
+    }
+
+    public class MipMap
+    {
+        int width;
+        int height;
+        byte[] data;
+
+        public int Width
+        {
+            get { return width; }
+            set { width = value; }
+        }
+
+        public int Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+
+        public byte[] Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+
+        public void Decompress()
+        {
+        }
+    }
+}
