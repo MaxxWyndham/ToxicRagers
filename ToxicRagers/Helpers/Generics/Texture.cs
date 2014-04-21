6a404a6842d6a1bf1c5d816d8860cc308ce564ee
Errol Errolson <errol@toxic-ragers.co.uk>
2014-04-21 04:11:17 +0100

    Generics > Texture:  Added extension and ShortFormat properties

--- ToxicRagers/Helpers/Generics/Texture.cs
+++ ToxicRagers/Helpers/Generics/Texture.cs
@@ -6,6 +6,7 @@ namespace ToxicRagers.Helpers
     public abstract class Texture
     {
         string name;
+        protected string extension;
         D3DFormat format;
         List<MipMap> mipMaps = new List<MipMap>();
 
@@ -19,6 +20,21 @@ namespace ToxicRagers.Helpers
         {
             get { return format; }
             set { format = value; }
+        }
+
+        public string ShortFormat
+        {
+            get
+            {
+                switch (format)
+                {
+                    case D3DFormat.A8R8G8B8:
+                        return "8888";
+
+                    default:
+                        return format.ToString().Substring(0, 4);
+                }
+            }
         }
 
         public List<MipMap> MipMaps { get { return mipMaps; } }
