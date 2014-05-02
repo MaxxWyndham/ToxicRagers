c5f4d63ca02870fc22b70ee3c37d03329341d07e
Errol Errolson <errol@toxic-ragers.co.uk>
2014-05-02 12:53:44 +0100

    Helpers.Logger: Added ResetLog()

--- ToxicRagers/Helpers/Logger.cs
+++ ToxicRagers/Helpers/Logger.cs
@@ -5,6 +5,13 @@ namespace ToxicRagers.Helpers
 {
     public static class Logger
     {
+        public static void ResetLog()
+        {
+            using (StreamWriter w = File.CreateText(Environment.CurrentDirectory + "\\Flummery.log"))
+            {
+            }
+        }
+
         public static void LogToFile(string format, params object[] args) {
             using (StreamWriter w = File.AppendText(Environment.CurrentDirectory + "\\Flummery.log"))
             {
