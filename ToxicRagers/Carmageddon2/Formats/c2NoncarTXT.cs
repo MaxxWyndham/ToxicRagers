cba666bbc2a99ae2d76fd6a2c7d78b9a6038fd5a
Errol Errolson <errol@toxic-ragers.co.uk>
2014-05-08 22:49:13 +0100

    C2 Noncar.txt:  I got bored.

---
+++ ToxicRagers/Carmageddon2/Formats/c2NoncarTXT.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ToxicRagers.Carmageddon2.Helpers;
+
+namespace ToxicRagers.Carmageddon2.Formats
+{
+    public class Noncar
+    {
+        public static Noncar Load(string path)
+        {
+            Noncar noncar = new Noncar();
+
+            using (var doc = new DocumentParser(path))
+            {
+            }
+
+            return noncar;
+        }
+    }
+}
---
+++ ToxicRagers/Carmageddon2/Helpers/DocumentParser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ToxicRagers.Carmageddon2.Helpers
+{
+    public class DocumentParser : BinaryReader
+    {
+        public DocumentParser(string Path) : base(new FileStream(Path, FileMode.Open), Encoding.ASCII) { }
+
+
+    }
+}
