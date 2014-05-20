815b6841ef93dfc0f0661b4299fe3b5243d5e1c9
Errol Errolson <errol@toxic-ragers.co.uk>
2014-05-20 08:55:03 +0100

    Core FBX:  The very early beginnings of an FBX parser

---
+++ ToxicRagers/Core/Formats/cFBX.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using ToxicRagers.Helpers;
+
+namespace ToxicRagers.Core.Formats
+{
+    public class FBX
+    {
+        public static FBX Load(string path)
+        {
+            // All these (int) casts are messy
+            FileInfo fi = new FileInfo(path);
+            Logger.LogToFile("{0}", path);
+            FBX fbx = new FBX();
+
+            using (var br = new BinaryReader(fi.OpenRead()))
+            {
+                if (br.ReadString(18) == "Kaydara FBX Binary")
+                {
+                    Logger.LogToFile("Binary FBX detected, unsupported!");
+                    return null;
+                }
+            }
+
+            using (var sr = new StreamReader(fi.OpenRead())) 
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+
+                    if (line.StartsWith(";")) { continue; } // Comment
+                }
+            }
+
+            return fbx;
+        }
+    }
+}
