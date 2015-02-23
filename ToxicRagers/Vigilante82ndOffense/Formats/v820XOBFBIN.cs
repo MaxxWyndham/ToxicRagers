a4201592614248881b98aab22f8d70ccbc305bfd
Errol Errolson <errol@toxic-ragers.co.uk>
2015-02-23 09:03:20 +0000

    Vigilante 82nd Offense: I really need to finish some of these other game importers

---
+++ ToxicRagers/Vigilante82ndOffense/Formats/v820XOBFBIN.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+using ToxicRagers.Helpers;
+
+namespace ToxicRagers.Vigilante82ndOffense.Formats
+{
+    public class BIN
+    {
+        List<int> aOffsets = new List<int>();
+
+        public static BIN Load(string path)
+        {
+            FileInfo fi = new FileInfo(path);
+            Logger.LogToFile("{0}", path);
+            BIN bin = new BIN();
+
+            using (var br = new BinaryReader(fi.OpenRead()))
+            {
+                int aCount = (int)br.ReadUInt32();
+                int aOffset = (int)br.ReadUInt32();
+                int bCount = (int)br.ReadUInt32();
+                int bOffset = (int)br.ReadUInt32();
+                int textureCount = (int)br.ReadUInt32();
+                int textureOffset = (int)br.ReadUInt32();
+                
+                int cCount = (int)br.ReadUInt32();
+                for (int i = 0; i < cCount; i++)
+                {
+                    br.ReadBytes(36);
+                }
+
+                Debug.Assert(br.BaseStream.Position == aOffset);
+
+                for (int i = 0; i < aCount; i++)
+                {
+                    bin.aOffsets.Add((int)br.ReadUInt32());
+                }
+
+                Debug.Assert((int)br.ReadUInt32() + aOffset == bOffset);
+
+                for (int i = 0; i < aCount; i++)
+                {
+                    br.BaseStream.Seek(aOffset + bin.aOffsets[i], SeekOrigin.Begin);
+
+                    Logger.LogToFile("###### {0,0:D4} ######", i);
+                    Logger.LogToFile("U: {0}", br.ReadUInt32());
+                    int ux = (int)br.ReadUInt32();
+                    Logger.LogToFile("{0}\t\t// Number of entries in second section", ux);
+                    Logger.LogToFile("U: {0}", br.ReadUInt32());
+                    int v = (int)br.ReadUInt32();
+                    Logger.LogToFile("{0}\t\t// Entry count", v);
+                    int offset1 = (int)br.ReadUInt32();
+                    Logger.LogToFile("{0}\t\t// Offset to first section", offset1);
+                    Logger.LogToFile("U: {0}", br.ReadUInt32());
+                    Logger.LogToFile("{0}\t// End of first section", br.ReadUInt32());
+                    int offset2 = (int)br.ReadUInt32();
+                    Logger.LogToFile("{0}\t// Offset to second section", offset2);
+                    Logger.LogToFile("U: {0}", br.ReadUInt32());
+                    Logger.LogToFile("U: {0}", br.ReadUInt32());
+                    Logger.LogToFile("U: {0}", br.ReadSingle());
+
+                    br.BaseStream.Seek(aOffset + bin.aOffsets[i] + offset1, SeekOrigin.Begin);
+                    for (int j = 0; j < v; j++)
+                    {
+                        Logger.LogToFile("\tPosition: X: {0} Y: {1} Z: {2} Normal: X: {3} Y: {4} Z: {5}", br.ReadSingle(), br.ReadSingle(), br.ReadSingle(), br.ReadSingle(), br.ReadSingle(), br.ReadSingle());
+                    }
+
+                    br.BaseStream.Seek(aOffset + bin.aOffsets[i] + offset2, SeekOrigin.Begin);
+                    for (int j = 0; j < ux; j++)
+                    {
+                        Logger.LogToFile("###### {0,0:D4}.{1,0:D4} ######", i, j);
+                        Logger.LogToFile("\t{0}", br.ReadUInt32());
+                        Logger.LogToFile("\t{0}", br.ReadUInt32());
+                        Logger.LogToFile("\t{0}", br.ReadUInt32());
+                        Logger.LogToFile("\t{0}", br.ReadUInt32());
+                        Logger.LogToFile("\t{0}", br.ReadUInt32());
+                        Logger.LogToFile("\t{0}", br.ReadUInt32());
+                        Logger.LogToFile("\t{0}", br.ReadUInt32());
+                        Logger.LogToFile("\t{0}", br.ReadUInt32());
+                        Logger.LogToFile("\t{0} {1} {2} {3}", br.ReadSingle(), br.ReadSingle(), br.ReadSingle(), br.ReadSingle());
+                        Logger.LogToFile("\t{0} {1} {2} {3}", br.ReadSingle(), br.ReadSingle(), br.ReadSingle(), br.ReadSingle());
+                        Logger.LogToFile("\t{0} {1} {2} {3}", br.ReadSingle(), br.ReadSingle(), br.ReadSingle(), br.ReadSingle());
+                        Logger.LogToFile("\t{0} {1}", br.ReadSingle(), br.ReadSingle());
+                        Logger.LogToFile("\t{0} {1} {2} {3}", br.ReadByte(), br.ReadByte(), br.ReadByte(), br.ReadByte());
+                        Logger.LogToFile("\t{0} {1} {2} {3}", br.ReadByte(), br.ReadByte(), br.ReadByte(), br.ReadByte());
+                    }
+                }
+
+                br.BaseStream.Seek(bOffset, SeekOrigin.Begin);
+
+                for (int i = 0; i < bCount; i++)
+                {
+                    br.ReadUInt32();    // bOffset + this value = start of entry
+                }
+
+                Debug.Assert((int)br.ReadUInt32() + bOffset == textureOffset);
+                br.BaseStream.Seek(textureOffset, SeekOrigin.Begin);
+
+                for (int i = 0; i < textureCount; i++)
+                {
+                    br.ReadUInt32();    // textureOffset + this value = start of entry
+                }
+
+                Debug.Assert((int)br.ReadUInt32() + textureOffset == br.BaseStream.Length);
+            }
+
+            return bin;
+        }
+    }
+}
---
+++ ToxicRagers/Vigilante82ndOffense/Formats/v82oEXP.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using ToxicRagers.Helpers;
+
+namespace ToxicRagers.Vigilante82ndOffense.Formats
+{
+    public enum EXPEntryType
+    {
+        Binary,
+        Animation
+    }
+
+    public class EXP
+    {
+        string name;
+        string location;
+        int entryCount;
+        List<EXPEntry> contents;
+
+        public string Name { get { return name; } }
+        public List<EXPEntry> Contents { get { return contents; } }
+
+        public EXP()
+        {
+            contents = new List<EXPEntry>();
+        }
+
+        public static EXP Load(string path)
+        {
+            FileInfo fi = new FileInfo(path);
+            Logger.LogToFile("{0}", path);
+            EXP exp = new EXP();
+
+            exp.name = Path.GetFileNameWithoutExtension(path);
+            exp.location = Path.GetDirectoryName(path) + "\\";
+
+            using (var br = new BEBinaryReader(fi.OpenRead()))
+            {
+                while (br.BaseStream.Position < br.BaseStream.Length)
+                {
+                    string section = br.ReadString(4);
+                    int length = 0;
+
+                    switch (section)
+                    {
+                        case "FORM":
+                            length = (int)br.ReadUInt32();
+                            break;
+
+                        case "TERR":
+                        case "XOBF":
+                            break;
+
+                        case "TITL":
+                            length = (int)br.ReadUInt32();
+                            if (length > 0) { throw new NotImplementedException(string.Format("TITL section has a length ({0}), don't know what to do!", length)); }
+                            break;
+
+                        case "TEXT":
+                            length = (int)br.ReadUInt32();
+                            if (length > 0) { throw new NotImplementedException(string.Format("TEXT section has a length ({0}), don't know what to do!", length)); }
+                            break;
+
+                        case "HEAD":
+                            length = (int)br.ReadUInt32();
+                            Logger.LogToFile("U: {0}", br.ReadUInt16());
+                            Logger.LogToFile("U: {0}", br.ReadUInt32());
+                            exp.entryCount = br.ReadUInt16();
+                            Logger.LogToFile("U: {0}", br.ReadUInt32());
+                            Logger.LogToFile("U: {0}", br.ReadUInt16());
+                            Logger.LogToFile("U: {0}", br.ReadUInt16());
+                            Logger.LogToFile("U: {0}", br.ReadUInt16());
+                            Logger.LogToFile("U: {0}", br.ReadUInt32());
+                            Logger.LogToFile("U: {0}", br.ReadUInt32());
+                            break;
+
+                        case "BIN ":
+                            length = (int)br.ReadUInt32();
+                            exp.contents.Add(new EXPEntry { Size = length, Offset = (int)br.BaseStream.Position, EntryType = EXPEntryType.Binary });
+                            br.ReadBytes(length);
+                            break;
+
+                        case "ANM ":
+                            length = (int)br.ReadUInt32();
+                            exp.contents.Add(new EXPEntry { Size = length, Offset = (int)br.BaseStream.Position, EntryType = EXPEntryType.Animation });
+                            br.ReadBytes(length);
+                            break;
+
+                        case "PLTX":
+                            length = (int)br.ReadUInt32();
+                            if (length > 0) { throw new NotImplementedException(string.Format("PLTX section has a length ({0}), don't know what to do!", length)); }
+                            break;
+
+                        default:
+                            throw new NotImplementedException(string.Format("Unexpected section \"{0}\" at {1,0:X0}", section, br.BaseStream.Position - 4));
+                    }
+                }
+            }
+
+            return exp;
+        }
+
+        public void Extract(EXPEntry file, string destination)
+        {
+            if (!Directory.Exists(destination)) { Directory.CreateDirectory(destination); }
+
+            using (var bw = new BinaryWriter(new FileStream(destination + "\\" + file.Filename, FileMode.Create)))
+            {
+                using (var fs = new FileStream(this.location + this.name + ".exp", FileMode.Open))
+                {
+                    fs.Seek(file.Offset, SeekOrigin.Begin);
+
+                    var buff = new byte[file.Size];
+                    fs.Read(buff, 0, file.Size);
+                    bw.Write(buff);
+                    buff = null;
+                }
+            }
+        }
+    }
+
+    public class EXPEntry
+    {
+        int offset;
+        int size;
+        EXPEntryType type;
+
+        public int Offset
+        {
+            get { return offset; }
+            set { offset = value; }
+        }
+
+        public int Size
+        {
+            get { return size; }
+            set { size = value; }
+        }
+
+        public EXPEntryType EntryType
+        {
+            get { return type; }
+            set { type = value; }
+        }
+
+        public string Filename { get { return string.Format("{0}.{1}", offset, type.ToString().Substring(0, 3).ToLower()); } }
+    }
+}
