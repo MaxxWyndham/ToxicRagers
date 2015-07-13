6fb4b1fde3e65324f183b439ea96fd57d62ad1e8
Maxx Wyndham <maxx@marketingradar.com>
2015-07-13 11:19:43 +0100

    C:R LOL: Added basic LOL file loading using unluac

---
+++ ToxicRagers/CarmageddonReincarnation/Formats/crLOL.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+using ToxicRagers.Helpers;
+
+using unluacNet;
+using unluac.decompile;
+using unluac.parse;
+
+namespace ToxicRagers.CarmageddonReincarnation.Formats
+{
+    public class LOL
+    {
+        string document = "";
+
+        public string Document
+        {
+            get { return document; }
+            set { document = value; }
+        }
+
+        public static LOL Load(string path)
+        {
+            FileInfo fi = new FileInfo(path);
+            Logger.LogToFile(Logger.LogLevel.Info, "{0}", path);
+            LOL lol = new LOL();
+
+            byte[] data = File.ReadAllBytes(path);
+
+            if (data[0] == 0x1b &&
+                data[1] == 0x4c &&
+                data[2] == 0x75 &&
+                data[3] == 0x61 &&
+                data[4] == 0x51)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                data[5] = 0;
+
+                ByteBuffer buffer = new ByteBuffer(data);
+                BHeader header = new BHeader(buffer);
+
+                Decompiler d = new Decompiler(header.function.parse(buffer, header));
+                d.decompile();
+                d.print(new Output((s) => { sb.Append(s); }, () => { sb.Append("\r\n"); }));
+
+                lol.document = sb.ToString();
+            }
+            else
+            {
+                lol.document = Encoding.Default.GetString(data);
+            }
+
+            return lol;
+        }
+    }
+}
