2f7123e7906d3330cddd6b50c44390b2f6d9717e
Maxx Wyndham <maxx@marketingradar.com>
2015-08-10 14:22:09 +0100

    C:R MT2: Added glow_in_the_dark_paint

---
+++ ToxicRagers/CarmageddonReincarnation/Formats/Materials/glow_in_the_dark_paint.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+using ToxicRagers.Helpers;
+
+namespace ToxicRagers.CarmageddonReincarnation.Formats.Materials
+{
+    public class glow_in_the_dark_paint : MT2
+    {
+        string normal;
+        Vector3 diffuseColour;
+        Vector3 specularColour;
+        Vector3 ambientLight;
+
+        public string Normal_Map
+        {
+            get { return normal; }
+            set { normal = value; }
+        }
+
+        public glow_in_the_dark_paint(XElement xml)
+            : base(xml)
+        {
+            var norm = xml.Descendants("Texture").Where(e => e.Attribute("Alias").Value == "Normal_Map").FirstOrDefault();
+
+            if (norm != null) { normal = norm.Attribute("FileName").Value; }
+
+            var diff = xml.Descendants("Constant").Where(e => e.Attribute("Alias").Value == "DiffuseColour").FirstOrDefault();
+            var spec = xml.Descendants("Constant").Where(e => e.Attribute("Alias").Value == "SpecularColour").FirstOrDefault();
+            var ambi = xml.Descendants("Constant").Where(e => e.Attribute("Alias").Value == "AmbientLight").FirstOrDefault();
+
+            if (diff != null) { diffuseColour = ReadConstant(diff); }
+            if (spec != null) { specularColour = ReadConstant(spec); }
+            if (ambi != null) { ambientLight = ReadConstant(ambi); }
+        }
+    }
+}
--- ToxicRagers/CarmageddonReincarnation/Formats/crMT2.cs
+++ ToxicRagers/CarmageddonReincarnation/Formats/crMT2.cs
@@ -280,7 +280,7 @@ namespace ToxicRagers.CarmageddonReincarnation.Formats
                 {
                     mt2 = (MT2)Activator.CreateInstance(Type.GetType("ToxicRagers.CarmageddonReincarnation.Formats.Materials." + basedOffOf, true, true), mt2.xml);
 
-                    if (basedOffOf.ToLower() == "vertex_norm_spec_env_base")
+                    if (basedOffOf.ToLower() == "glow_in_the_dark_paint")
                     {
                         Logger.LogToFile(Logger.LogLevel.Info, path);
                         //Logger.LogToFile(Logger.LogLevel.Info, mt2.ToString());
