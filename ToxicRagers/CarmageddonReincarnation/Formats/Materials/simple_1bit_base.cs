08db465669cff49f504f6f434dd46741839e1d38
Maxx Wyndham <maxx@marketingradar.com>
2015-08-10 14:48:32 +0100

    C:R MT2: Added simple_1bit_base

---
+++ ToxicRagers/CarmageddonReincarnation/Formats/Materials/simple_1bit_base.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace ToxicRagers.CarmageddonReincarnation.Formats.Materials
+{
+    public class simple_1bit_base : MT2
+    {
+        // Uses:
+        // AlphaCutOff
+
+        public simple_1bit_base(XElement xml)
+            : base(xml)
+        {
+            var diff = xml.Descendants("Texture").Where(e => e.Attribute("Alias").Value == "DiffuseColour").FirstOrDefault();
+
+            if (diff != null) { diffuse = diff.Attribute("FileName").Value; }
+        }
+    }
+}
