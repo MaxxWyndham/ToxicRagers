3c9862e70caea653a04b1e3b85698224103c4206
Maxx Wyndham <maxx@marketingradar.com>
2015-08-10 15:58:14 +0100

    C:R MT2: Added simple_norm_spec_env_unlit_base

---
+++ ToxicRagers/CarmageddonReincarnation/Formats/Materials/simple_norm_spec_env_unlit_base.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace ToxicRagers.CarmageddonReincarnation.Formats.Materials
+{
+    public class simple_norm_spec_env_unlit_base : MT2
+    {
+        // Uses:
+        // ReflectionMultiplier
+
+        string normal;
+        string specular;
+
+        public string Normal_Map
+        {
+            get { return normal; }
+            set { normal = value; }
+        }
+
+        public string Spec_Map
+        {
+            get { return specular; }
+            set { specular = value; }
+        }
+
+        public simple_norm_spec_env_unlit_base(XElement xml)
+            : base(xml)
+        {
+            var diff = xml.Descendants("Texture").Where(e => e.Attribute("Alias").Value == "DiffuseColour").FirstOrDefault();
+            var norm = xml.Descendants("Texture").Where(e => e.Attribute("Alias").Value == "Normal_Map").FirstOrDefault();
+            var spec = xml.Descendants("Texture").Where(e => e.Attribute("Alias").Value == "Spec_Map").FirstOrDefault();
+
+            if (diff != null) { diffuse = diff.Attribute("FileName").Value; }
+            if (norm != null) { normal = norm.Attribute("FileName").Value; }
+            if (spec != null) { specular = spec.Attribute("FileName").Value; }
+        }
+    }
+}
--- ToxicRagers/CarmageddonReincarnation/Formats/crMT2.cs
+++ ToxicRagers/CarmageddonReincarnation/Formats/crMT2.cs
@@ -298,7 +298,7 @@ namespace ToxicRagers.CarmageddonReincarnation.Formats
                 {
                     mt2 = (MT2)Activator.CreateInstance(Type.GetType("ToxicRagers.CarmageddonReincarnation.Formats.Materials." + basedOffOf, true, true), mt2.xml);
 
-                    if (basedOffOf.ToLower() == "decal_base")
+                    if (basedOffOf.ToLower() == "simple_norm_spec_env_unlit_base")
                     {
                         Logger.LogToFile(Logger.LogLevel.Info, path);
                         //Logger.LogToFile(Logger.LogLevel.Info, mt2.ToString());
