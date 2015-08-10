6efcbad33b2ddc1539dc127a357e29854d0e01d6
Maxx Wyndham <maxx@marketingradar.com>
2015-08-10 12:03:31 +0100

    C:R MT2: Added simple_spec_base

---
+++ ToxicRagers/CarmageddonReincarnation/Formats/Materials/simple_spec_base.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace ToxicRagers.CarmageddonReincarnation.Formats.Materials
+{
+    public class simple_spec_base : MT2
+    {
+        // Uses:
+        // NeedsWorldSpaceVertexNormal, NeedsWorldEyePos, NeedsWorldVertexPos, NeedsWorldLightDir
+        // Fresnel_R0, ReflectionMultiplier
+
+        string specular;
+
+        public string Spec_Map
+        {
+            get { return specular; }
+            set { specular = value; }
+        }
+
+        public simple_spec_base(XElement xml)
+            : base(xml)
+        {
+            var diff = xml.Descendants("Texture").Where(e => e.Attribute("Alias").Value == "DiffuseColour").FirstOrDefault();
+            var spec = xml.Descendants("Texture").Where(e => e.Attribute("Alias").Value == "Spec_Map").FirstOrDefault();
+
+            if (diff != null) { diffuse = diff.Attribute("FileName").Value; }
+            if (spec != null) { specular = spec.Attribute("FileName").Value; }
+        }
+    }
+}
--- ToxicRagers/CarmageddonReincarnation/Formats/crMT2.cs
+++ ToxicRagers/CarmageddonReincarnation/Formats/crMT2.cs
@@ -271,7 +271,7 @@ namespace ToxicRagers.CarmageddonReincarnation.Formats
                 {
                     mt2 = (MT2)Activator.CreateInstance(Type.GetType("ToxicRagers.CarmageddonReincarnation.Formats.Materials." + basedOffOf, true, true), mt2.xml);
 
-                    if (basedOffOf.ToLower() == "simple_norm_spec_env_base")
+                    if (basedOffOf.ToLower() == "simple_spec_base")
                     {
                         Logger.LogToFile(Logger.LogLevel.Info, path);
                         //Logger.LogToFile(Logger.LogLevel.Info, mt2.ToString());
