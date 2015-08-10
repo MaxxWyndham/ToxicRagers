219e309f9cb737a56870fe430738867085f9a794
Maxx Wyndham <maxx@marketingradar.com>
2015-08-10 14:54:35 +0100

    C:R MT2: Added car_shader_no_normals_base

---
+++ ToxicRagers/CarmageddonReincarnation/Formats/Materials/car_shader_no_normals_base.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace ToxicRagers.CarmageddonReincarnation.Formats.Materials
+{
+    public class car_shader_no_normals_base : MT2
+    {
+        // Uses:
+        // DoubleSided
+        // ReflectionBluryness, ReflectionMultiplier, Fresnel_R0
+
+        string normals;
+        string decals;
+        string decalsSpec;
+
+        public string Normal_Map
+        {
+            get { return normals; }
+            set { normals = value; }
+        }
+
+        public string Decals
+        {
+            get { return decals; }
+            set { decals = value; }
+        }
+
+        public string DecalsSpec
+        {
+            get { return decalsSpec; }
+            set { decalsSpec = value; }
+        }
+
+        public car_shader_no_normals_base(XElement xml)
+            : base(xml)
+        {
+            var norm = xml.Descendants("Texture").Where(e => e.Attribute("Alias").Value == "Normal_Map").FirstOrDefault();
+            var deca = xml.Descendants("Texture").Where(e => e.Attribute("Alias").Value == "Decals").FirstOrDefault();
+            var decs = xml.Descendants("Texture").Where(e => e.Attribute("Alias").Value == "DecalsSpec").FirstOrDefault();
+
+            if (norm != null) { normals = norm.Attribute("FileName").Value; }
+            if (deca != null) { decals = deca.Attribute("FileName").Value; }
+            if (decs != null) { decalsSpec = decs.Attribute("FileName").Value; }
+        }
+    }
+}
--- ToxicRagers/CarmageddonReincarnation/Formats/crMT2.cs
+++ ToxicRagers/CarmageddonReincarnation/Formats/crMT2.cs
@@ -289,7 +289,7 @@ namespace ToxicRagers.CarmageddonReincarnation.Formats
                 {
                     mt2 = (MT2)Activator.CreateInstance(Type.GetType("ToxicRagers.CarmageddonReincarnation.Formats.Materials." + basedOffOf, true, true), mt2.xml);
 
-                    if (basedOffOf.ToLower() == "simple_1bit_base")
+                    if (basedOffOf.ToLower() == "car_shader_no_normals_base")
                     {
                         Logger.LogToFile(Logger.LogLevel.Info, path);
                         //Logger.LogToFile(Logger.LogLevel.Info, mt2.ToString());
