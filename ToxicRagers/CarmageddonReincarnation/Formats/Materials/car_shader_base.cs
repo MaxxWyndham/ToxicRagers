b610e2abe8424c6964f18e3f5f3ef8677280d261
Maxx Wyndham <maxx@marketingradar.com>
2015-08-10 15:05:56 +0100

    C:R MT2: Added car_shader_base

---
+++ ToxicRagers/CarmageddonReincarnation/Formats/Materials/car_shader_base.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace ToxicRagers.CarmageddonReincarnation.Formats.Materials
+{
+    public class car_shader_base : MT2
+    {
+        // Uses:
+        // ReflectionBluryness, Fresnel_R0
+
+        string normals;
+        string decals;
+        string decalsSpec;
+
+        public string Normal_Map
+        {
+            get { return normals; }
+            set { normals = value; }
+        }
+
+        public string Decals
+        {
+            get { return decals; }
+            set { decals = value; }
+        }
+
+        public string DecalsSpec
+        {
+            get { return decalsSpec; }
+            set { decalsSpec = value; }
+        }
+
+        public car_shader_base(XElement xml)
+            : base(xml)
+        {
+            var norm = xml.Descendants("Texture").Where(e => e.Attribute("Alias").Value == "Normal_Map").FirstOrDefault();
+            var deca = xml.Descendants("Texture").Where(e => e.Attribute("Alias").Value == "Decals").FirstOrDefault();
+            var decs = xml.Descendants("Texture").Where(e => e.Attribute("Alias").Value == "DecalsSpec").FirstOrDefault();
+
+            if (norm != null) { normals = norm.Attribute("FileName").Value; }
+            if (deca != null) { decals = deca.Attribute("FileName").Value; }
+            if (decs != null) { decalsSpec = decs.Attribute("FileName").Value; }
+        }
+    }
+}
