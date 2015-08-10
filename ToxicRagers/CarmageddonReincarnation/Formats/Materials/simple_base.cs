22c93c062e71617257b3055289820a99e478ad64
Maxx Wyndham <maxx@marketingradar.com>
2015-08-10 13:42:17 +0100

    C:R MT2: Added simple_base

---
+++ ToxicRagers/CarmageddonReincarnation/Formats/Materials/simple_base.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace ToxicRagers.CarmageddonReincarnation.Formats.Materials
+{
+    public class simple_base : MT2
+    {
+        // Uses:
+        // Walkable
+        // EmissiveFactor
+
+        public simple_base(XElement xml)
+            : base(xml)
+        {
+            var diff = xml.Descendants("Texture").Where(e => e.Attribute("Alias").Value == "DiffuseColour").FirstOrDefault();
+
+            if (diff != null) { diffuse = diff.Attribute("FileName").Value; }
+        }
+    }
+}
