b481f6f8071ac3266bea30bde8eb4a9ab9b55fbd
Maxx Wyndham <maxx@marketingradar.com>
2015-09-04 12:11:21 +0100

    Stainless ZAD: Now ignores zad, zip and img files.  Allows tdx

--- ToxicRagers/Stainless/Formats/sZAD.cs
+++ ToxicRagers/Stainless/Formats/sZAD.cs
@@ -154,7 +154,8 @@ namespace ToxicRagers.Stainless.Formats
             foreach (var file in Directory.GetFiles(path))
             {
                 if (string.Compare(Path.GetExtension(file), ".zad", true) == 0) { continue; }
-                if (string.Compare(Path.GetExtension(file), ".tdx", true) == 0) { continue; }
+                if (string.Compare(Path.GetExtension(file), ".zip", true) == 0) { continue; }
+                if (string.Compare(Path.GetExtension(file), ".img", true) == 0) { continue; }
 
                 ZADEntry entry = new ZADEntry();
                 entry.Data = File.ReadAllBytes(file);
