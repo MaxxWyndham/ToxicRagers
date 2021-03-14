21007a1a5b0a1b2d7bb79f22e63e2d39acd7e77c
Maxx Wyndham <maxx@marketingradar.com>
2021-03-14 14:31:29 +0000

    C1 PIX:  Added Save functionality for C1 PIX archives

--- ToxicRagers/Carmageddon/Formats/c1PIX.cs
+++ ToxicRagers/Carmageddon/Formats/c1PIX.cs
@@ -5,21 +5,19 @@ using System.IO;
 using System.Runtime.InteropServices;
 using System.Text;
 
+using ToxicRagers.Carmageddon.Helpers;
 using ToxicRagers.Helpers;
 
 namespace ToxicRagers.Carmageddon.Formats
 {
     public class PIX
     {
+        public static Game GamePalette { get; } = new Game();
+
         public static int[] RawPalette = { 16711900, 6095619, 9634306, 13172993, 16711680, 16728128, 16744319, 16760767, 4002056, 7214854, 10362372, 13575170, 16722432, 16736321, 16684930, 16698819, 4332553, 7416327, 10565893, 13649666, 16733440, 16743482, 16687732, 16697774, 5911309, 8603146, 11360519, 14052355, 16744192, 16752189, 16694395, 16702392, 4862728, 7819270, 10841604, 13798146, 16754688, 16760384, 16766081, 16771777, 4865801, 7824647, 10848773, 13807618, 16766208, 16703299, 16640645, 16577736, 5394954, 8224008, 11118853, 13947907, 16776960, 16711244, 16711319, 16645603, 4608526, 6978827, 9349383, 11654148, 14024448, 14745158, 15400331, 16121041, 2966528, 5010176, 7053824, 9031680, 11075328, 12648263, 14220941, 15793876, 5131591, 7367765, 9538404, 11774322, 13944960, 14669212, 15328184, 16052436, 1591296, 2586112, 3580672, 4575488, 5570048, 8257085, 10943867, 13630904, 402944, 2640929, 4813377, 7051362, 9223810, 10736538, 12314802, 13827530, 16917, 28965, 41269, 53316, 65364, 4259711, 8388265, 12582612, 17185, 1925439, 3899228, 5807226, 7715479, 9554605, 11393732, 13232858, 15143, 27719, 40296, 52872, 65448, 4194238, 8323027, 12451817, 10016, 934457, 1793362, 2717802, 3576707, 5810330, 8109490, 10343113, 13878, 26728, 39835, 52685, 65535, 4194046, 8388350, 12516861, 10031, 21347, 32407, 43723, 54783, 4186110, 8382974, 12514301, 9786, 18283, 26525, 35022, 43263, 3980799, 7983615, 11921151, 6712, 12906, 18844, 25037, 30975, 3248895, 6532607, 9750527, 60, 109, 158, 206, 255, 3816191, 7566335, 11382271, 8072704, 9652509, 11232571, 12746840, 14326645, 15120283, 15979712, 16773350, 2230272, 4988432, 7746848, 10439472, 13197632, 13270115, 13342341, 13414824, 5321499, 6503453, 7751199, 8933153, 10115107, 12282666, 14515505, 16683064, 6176801, 7620633, 9064466, 10442506, 11886338, 13468725, 15116904, 16699291, 3745551, 5061411, 6443064, 7758924, 9074784, 10588282, 12102036, 13615534, 2827800, 3946022, 5064244, 6182465, 7300687, 9537649, 11708818, 13945780, 5423, 1189439, 2438991, 3623006, 4807022, 6911883, 8951463, 11056324, 1315860, 2171169, 3026478, 3947324, 4802889, 5658198, 6513507, 7368816, 8289662, 9144971, 10000280, 10855589, 11711154, 12632000, 13487309, 14342618, 0, 1119273, 2237752, 3355976, 4474455, 5592934, 6711413, 7829893, 8948116, 10066595, 11185074, 12303553, 13421777, 14540256, 15658735, 16777215 };
         static Color[] palette;
-        List<PIXIE> pixies;
 
-        public List<PIXIE> Pixies
-        {
-            get => pixies;
-            set => pixies = value;
-        }
+        public List<PIXIE> Pixies { get; set; } = new List<PIXIE>();
 
         public static Color[] Palette
         {
@@ -36,11 +34,6 @@ namespace ToxicRagers.Carmageddon.Formats
 
                 return palette;
             }
-        }
-
-        public PIX()
-        {
-            pixies = new List<PIXIE>();
         }
 
         public static PIX Load(string path)
@@ -96,7 +89,7 @@ namespace ToxicRagers.Carmageddon.Formats
 
                         case 0x22:  // 34
                         case 0x00:  // 0
-                            pix.pixies.Add(pixies.Pop());
+                            pix.Pixies.Add(pixies.Pop());
                             break;
 
                         case 0x3d:
@@ -120,6 +113,41 @@ namespace ToxicRagers.Carmageddon.Formats
 
             return pix;
         }
+
+        public void Save(string path)
+        {
+            using (FileStream fs = new FileStream(path, FileMode.Create))
+            using (BEBinaryWriter bw = new BEBinaryWriter(fs))
+            {
+                bw.WriteInt32(0x12);
+                bw.WriteInt32(0x8);
+                bw.WriteInt32(0x2);
+                bw.WriteInt32(0x2);
+
+                foreach (PIXIE pixie in Pixies)
+                {
+                    bw.WriteInt32(0x3);
+                    bw.WriteInt32(12 + pixie.Name.Length);
+                    bw.WriteByte((byte)pixie.Format);
+                    bw.WriteInt16(pixie.RowSize);
+                    bw.WriteInt16(pixie.Width);
+                    bw.WriteInt16(pixie.Height);
+                    bw.WriteInt16(pixie.HalfWidth);
+                    bw.WriteInt16(pixie.HalfHeight);
+                    bw.WriteString(pixie.Name.ToUpper());
+                    bw.WriteByte(0);
+
+                    bw.WriteInt32(0x21);
+                    bw.WriteInt32(8 + (pixie.RowSize * pixie.Height));
+                    bw.WriteInt32(pixie.PixelCount);
+                    bw.WriteInt32(pixie.PixelSize);
+                    bw.Write(pixie.Data);
+
+                    bw.WriteInt32(0x0);
+                    bw.WriteInt32(0);
+                }
+            }
+        }
     }
 
     public class PIXIE
@@ -132,136 +160,135 @@ namespace ToxicRagers.Carmageddon.Formats
             C2_16bitAlpha = 18
         }
 
-        string name;
-        PixelmapFormat format;
-        int width;
-        int height;
-        int halfWidth;
-        int halfHeight;
-        int rowSize;
-        int pixelCount;
-        int pixelSize;
-        byte[] data;
+        public byte[] Data { get; private set; }
 
-        public PixelmapFormat Format
-        {
-            get => format;
-            set => format = value;
-        }
+        public PixelmapFormat Format { get; set; }
 
-        public int Width
-        {
-            get => width;
-            set => width = value;
-        }
+        public int Width { get; set; }
 
-        public int Height
-        {
-            get => height;
-            set => height = value;
-        }
+        public int Height { get; set; }
 
-        public int HalfWidth
-        {
-            get => halfWidth;
-            set => halfWidth = value;
-        }
+        public int HalfWidth { get; set; }
 
-        public int HalfHeight
-        {
-            get => halfHeight;
-            set => halfHeight = value;
-        }
+        public int HalfHeight { get; set; }
 
-        public int RowSize
-        {
-            get => rowSize;
-            set => rowSize = value;
-        }
+        public int RowSize { get; set; }
 
         public int ActualRowSize
         {
             get
             {
-                if (pixelCount != height * width || rowSize > width)
+                if (PixelCount != Height * Width || RowSize > Width)
                 {
-                    return rowSize / pixelSize;
+                    return RowSize / PixelSize;
                 }
                 else
                 {
-                    return rowSize;
+                    return RowSize;
                 }
             }
         }
 
-        public int PixelCount
-        {
-            get => pixelCount;
-            set => pixelCount = value;
-        }
+        public int PixelCount { get; set; }
 
-        public int PixelSize
-        {
-            get => pixelSize;
-            set => pixelSize = value;
-        }
+        public int PixelSize { get; set; }
 
-        public string Name
-        {
-            get => name;
-            set => name = value;
-        }
+        public string Name { get; set; }
 
         public int DataLength
         {
             get
             {
-                if (format == PixelmapFormat.Palette)
+                if (Format == PixelmapFormat.Palette)
                 {
                     return 4 * 256;
                 }
                 else
                 {
-                    return height * ActualRowSize * pixelSize;
+                    return Height * ActualRowSize * PixelSize;
                 }
             }
         }
 
         public void SetData(byte[] data)
         {
-            this.data = data;
+            Data = data;
         }
 
         public Color GetColourAtPixel(int x, int y)
         {
-            switch (format)
+            switch (Format)
             {
                 case PixelmapFormat.C1_8bit:
-                    return PIX.Palette[data[x + y * ActualRowSize]];
+                    return PIX.Palette[Data[x + y * ActualRowSize]];
 
                 case PixelmapFormat.C2_16bit:
-                    return ColourHelper.R5G6B5ToColour((data[x + (y * rowSize)] << 8) | data[x + (y * rowSize) + 1]);
+                    return ColourHelper.R5G6B5ToColour((Data[x + (y * RowSize)] << 8) | Data[x + (y * RowSize) + 1]);
 
                 case PixelmapFormat.C2_16bitAlpha:
-                    return ColourHelper.A4R4G4B4ToColour(data[x + (y * rowSize)] << 8 | data[x + (y * rowSize) + 1]);
+                    return ColourHelper.A4R4G4B4ToColour(Data[x + (y * RowSize)] << 8 | Data[x + (y * RowSize) + 1]);
             }
 
             return Color.Pink;
         }
 
+        public static PIXIE FromBitmap(PixelmapFormat format, Bitmap bitmap)
+        {
+            PIXIE pixie = new PIXIE
+            {
+                Format = format,
+                Width = bitmap.Width,
+                Height = bitmap.Height,
+                HalfWidth = bitmap.Width / 2,
+                HalfHeight = bitmap.Height / 2,
+                RowSize = bitmap.Width
+            };
+
+            if (pixie.Width % 2 != 0 || pixie.HalfWidth % 2 != 0)
+            {
+                pixie.RowSize = (pixie.HalfWidth + (pixie.HalfWidth % 2 == 0 ? 2 : 1)) * 2;
+            }
+
+            pixie.PixelCount = pixie.RowSize * pixie.Height;
+            pixie.PixelSize = 1;
+
+            byte[] data = new byte[pixie.RowSize * pixie.Height];
+            Dictionary<Colour, byte> lut = new Dictionary<Colour, byte>();
+
+            for (int y = 0; y < bitmap.Height; y++)
+            {
+                for (int x = 0; x < pixie.Width; x++)
+                {
+                    Colour c = Colour.FromColor(bitmap.GetPixel(x, y));
+
+                    if (!lut.TryGetValue(c, out byte index)) 
+                    {
+                        index = (byte)PIX.GamePalette.GetNearestPixelIndex(c);
+                        lut.Add(c, index);
+                    }
+
+                    data[y * pixie.RowSize + x] = index;
+                }
+            }
+
+            pixie.SetData(data);
+
+            return pixie;
+        }
+
         public Bitmap GetBitmap()
         {
-            Bitmap bmp = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+            Bitmap bmp = new Bitmap(Width, Height, PixelFormat.Format32bppArgb);
             {
-                BitmapData bmpdata = bmp.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
+                BitmapData bmpdata = bmp.LockBits(new Rectangle(0, 0, Width, Height), ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
 
                 using (MemoryStream nms = new MemoryStream())
                 {
-                    for (int y = 0; y < height; y++)
+                    for (int y = 0; y < Height; y++)
                     {
                         for (int x = 0; x < ActualRowSize; x++)
                         {
-                            Color c = GetColourAtPixel(x * pixelSize, y);
+                            Color c = GetColourAtPixel(x * PixelSize, y);
                             nms.WriteByte(c.B);
                             nms.WriteByte(c.G);
                             nms.WriteByte(c.R);
