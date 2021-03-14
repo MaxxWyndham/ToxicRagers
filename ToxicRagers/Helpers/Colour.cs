a9a339cf8274b4963903be2f4e76bdf997894d1f
Maxx Wyndham <maxx@marketingradar.com>
2021-03-14 14:30:50 +0000

    Core:  Added the concept of Palettes and nearest distance to colour

--- ToxicRagers/Helpers/Colour.cs
+++ ToxicRagers/Helpers/Colour.cs
@@ -5,22 +5,70 @@ namespace ToxicRagers.Helpers
 {
     public class Colour
     {
-        public float R { get; set; }
+        public float R { get; }
 
-        public float G { get; set; }
+        public float G { get; }
 
-        public float B { get; set; }
+        public float B { get; }
 
-        public float A { get; set; }
+        public float A { get; }
+
+        public float H { get; }
+
+        public float S { get; }
+
+        public float L { get; }
+
+        public static Colour Black => new Colour(0, 0, 0, 1);
 
         public static Colour White => new Colour(1, 1, 1, 1);
 
         public Colour(float r, float g, float b, float a)
         {
+            if (r > 1f) { throw new ArgumentOutOfRangeException("r", "r is expected in the 0.0 to 1.0f range"); }
+            if (g > 1f) { throw new ArgumentOutOfRangeException("g", "g is expected in the 0.0 to 1.0f range"); }
+            if (b > 1f) { throw new ArgumentOutOfRangeException("b", "b is expected in the 0.0 to 1.0f range"); }
+            if (a > 1f) { throw new ArgumentOutOfRangeException("a", "a is expected in the 0.0 to 1.0f range"); }
+
             R = r;
             G = g;
             B = b;
             A = a;
+
+            //float min = Math.Min(r, Math.Min(g, b));
+            //float max = Math.Max(r, Math.Max(g, b));
+            //string maxChannel = max == r ? "R" : max == g ? "G" : "B";
+
+            //L = (min + max) / 2f;
+
+            //if (min != max)
+            //{
+            //    if (L <= 0.5f)
+            //    {
+            //        S = (max - min) / (max + min);
+            //    }
+            //    else
+            //    {
+            //        S = (max - min) / (2f - max - min);
+            //    }
+
+            //    switch (maxChannel)
+            //    {
+            //        case "R":
+            //            H = (g - b) / (max - min) + (g < b ? 6 : 0);
+            //            break;
+
+            //        case "G":
+            //            H = (b - r) / (max - min) + 2;
+            //            break;
+
+            //        case "B":
+            //            H = (r - g) / (max - min) + 4;
+            //            break;
+            //    }
+
+            //    //H *= 60;
+            //}
         }
 
         public static Colour FromArgb(float a, float r, float g, float b)
@@ -28,12 +76,27 @@ namespace ToxicRagers.Helpers
             return new Colour(r, g, b, a);
         }
 
+        public static Colour FromArgb(byte a, byte r, byte g, byte b)
+        {
+            return new Colour(r / 255f, g / 255f, b / 255f, a / 255f);
+        }
+
+        public static Colour FromRgb(byte r, byte g, byte b)
+        {
+            return new Colour(r / 255.0f, g / 255.0f, b / 255.0f, 1);
+        }
+
+        public static Colour FromColor(Color c)
+        {
+            return new Colour(c.R / 255f, c.G / 255f, c.B / 255f, c.A / 255f);
+        }
+
         public static Colour Random()
         {
             KnownColor[] knownColourNames = (KnownColor[])Enum.GetValues(typeof(KnownColor));
             Color c = Color.FromKnownColor(knownColourNames[ToxicRagers.Random.Next(knownColourNames.Length)]);
 
-            return new Colour(c.R, c.G, c.B, c.A);
+            return Colour.FromArgb(c.A, c.R, c.G, c.B);
         }
     }
 }
---
+++ ToxicRagers/Helpers/Palette.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ToxicRagers.Helpers
+{
+    public class Palette : List<Colour>
+    {
+        public int GetNearestPixelIndex(Colour c)
+        {
+            float smallestDiff = float.MaxValue;
+            int index = -1;
+
+            for (int i = 0; i < Count; i++)
+            {
+                Colour p = this[i];
+
+                //float hdiff = Math.Min(Math.Abs(p.H - c.H), Math.Abs(p.H - c.H + (p.H < c.H ? -360f : 360f))) * 1.2f;
+                //float sdiff = (p.S - c.S) * 1.5f;
+                //float ldiff = p.L - c.L;
+                //if (p.L < c.L) { ldiff *= 2f; }
+
+                //hdiff *= hdiff;
+                //sdiff *= sdiff;
+                //ldiff *= ldiff;
+
+                //float currentDiff = hdiff + sdiff + ldiff;
+
+                float currentDiff = (float)(Math.Pow((c.R - p.R) * 0.299f, 2) + Math.Pow((c.G - p.G) * 0.587f, 2) + Math.Pow((c.B - p.B) * 0.114f, 2));
+
+                if (currentDiff < smallestDiff)
+                {
+                    smallestDiff = currentDiff;
+                    index = i;
+                }
+            }
+
+            return index;
+        }
+    }
+}
