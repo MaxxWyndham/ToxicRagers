4ed7374c6ffaf83dcddb3678906c9ab0713d9a53
Maxx Wyndham <maxx@marketingradar.com>
2021-03-18 15:56:12 +0000

    C1 and C2:  Parsers for Opponent.txt files

---
+++ ToxicRagers/Carmageddon/Formats/c1OpponentTXT.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections.Generic;
+
+using ToxicRagers.Carmageddon.Helpers;
+using ToxicRagers.Helpers;
+
+namespace ToxicRagers.Carmageddon.Formats
+{
+    public enum NetworkAvailability
+    {
+        never,
+        eagle,
+        hawk,
+        all
+    }
+
+    public class OpponentTXT
+    {
+        public List<OpponentDetails> Opponents = new List<OpponentDetails>();
+
+        public static OpponentTXT Load(string path)
+        {
+            DocumentParser file = new DocumentParser(path);
+            OpponentTXT opponents = new OpponentTXT();
+
+            int numOpponents = file.ReadInt();
+
+            for (int i = 0; i < numOpponents; ++i)
+            {
+                OpponentDetails opponent = new OpponentDetails
+                {
+                    DriverName = file.ReadLine(),
+                    DriverShortName = file.ReadLine(),
+                    CarNumber = file.ReadInt(),
+                    StrengthRating = file.ReadInt(),
+                    NetworkAvailability = file.ReadEnum<NetworkAvailability>(),
+                    MugshotName = file.ReadLine(),
+                    CarFilename = file.ReadLine(),
+                    StolenCarFlicName = file.ReadLine()
+                };
+
+                int textChunks = file.ReadInt();
+
+                for (int j = 0; j < textChunks; j++)
+                {
+                    TextChunk chunk = new TextChunk
+                    {
+                        Position = file.ReadVector2(),
+                        Frame = file.ReadVector2()
+                    };
+
+                    int chunkLines = file.ReadInt();
+
+                    while (chunkLines > 8) { file.ReadLine(); chunkLines--; }
+
+                    for (int k = 0; k < chunkLines; k++)
+                    {
+                        chunk.Lines.Add(file.ReadLine());
+                    }
+
+                    opponent.TextChunks.Add(chunk);
+                }
+
+                opponents.Opponents.Add(opponent);
+            }
+
+            return opponents;
+        }
+    }
+
+    public class OpponentDetails
+    {
+        public string DriverName { get; set; }
+
+        public string DriverShortName { get; set; }
+
+        public int CarNumber { get; set; }
+
+        public int StrengthRating { get; set; }
+
+        public NetworkAvailability NetworkAvailability { get; set; }
+
+        public string MugshotName { get; set; }
+
+        public string CarFilename { get; set; }
+
+        public string StolenCarFlicName { get; set; }
+
+        public List<TextChunk> TextChunks { get; set; } = new List<TextChunk>();
+
+        // Helpers
+
+        public string TopSpeed => TextChunks.Count > 0 && TextChunks[0].Lines.Count > 0 ? TextChunks[0].Lines[0] : null;
+
+        public string KerbWeight => TextChunks.Count > 0 && TextChunks[0].Lines.Count > 1 ? TextChunks[0].Lines[1] : null;
+
+        public string To60 => TextChunks.Count > 0 && TextChunks[0].Lines.Count > 2 ? TextChunks[0].Lines[2] : null;
+
+        public string Bio => TextChunks.Count > 1 && TextChunks[1].Lines.Count > 0 ? string.Join(" ", TextChunks[1].Lines) : null;
+    }
+
+    public class TextChunk
+    {
+        public Vector2 Position { get; set; }
+
+        public Vector2 Frame { get; set; }
+
+        public List<string> Lines { get; set; } = new List<string>();
+    }
+}
---
+++ ToxicRagers/Carmageddon2/Formats/c2OpponentTXT.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+
+using ToxicRagers.Carmageddon.Helpers;
+
+namespace ToxicRagers.Carmageddon2.Formats
+{
+    public enum NetworkAvailability
+    {
+        eagle,
+        all
+    }
+
+    public class OpponentTXT
+    {
+        public List<OpponentDetails> Opponents = new List<OpponentDetails>();
+
+        public static OpponentTXT Load(string path)
+        {
+            DocumentParser file = new DocumentParser(path);
+            OpponentTXT opponents = new OpponentTXT();
+
+            int numOpponents = file.ReadInt();
+
+            for (int i = 0; i < numOpponents; ++i)
+            {
+                opponents.Opponents.Add(new OpponentDetails
+                {
+                    DriverName = file.ReadLine(),
+                    DriverShortName = file.ReadLine(),
+                    CarName = file.ReadLine(),
+                    StrengthRating = file.ReadInt(),
+                    CostToBuy = file.ReadInt(),
+                    NetworkAvailability = file.ReadEnum<NetworkAvailability>(),
+                    CarFilename = file.ReadLine(),
+                    TopSpeed = file.ReadLine(),
+                    KerbWeight = file.ReadLine(),
+                    To60 = file.ReadLine(),
+                    Bio = file.ReadLine()
+                });
+            }
+
+            return opponents;
+        }
+    }
+
+    public class OpponentDetails
+    {
+        public string DriverName { get; set; }
+
+        public string DriverShortName { get; set; }
+
+        public string CarName { get; set; }
+
+        public int StrengthRating { get; set; }
+
+        public int CostToBuy { get; set; }
+
+        public NetworkAvailability NetworkAvailability { get; set; }
+
+        public string CarFilename { get; set; }
+
+        public string TopSpeed { get; set; }
+
+        public string KerbWeight { get; set; }
+
+        public string To60 { get; set; }
+
+        public string Bio { get; set; }
+    }
+}
