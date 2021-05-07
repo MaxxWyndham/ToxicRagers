8c65aaf719bdc7c667987c680996e1814a7f22f2
Maxx Wyndham <maxx@marketingradar.com>
2021-05-07 16:52:59 +0100

    C2 TWT:  Creating, Adding and Extracting

---
+++ ToxicRagers.Tests/Carmageddon2/Formats/c2TWT.cs
@@ -0,0 +1,28 @@
+﻿using TR = ToxicRagers.Carmageddon2.Formats;
+
+using Xunit;
+
+namespace ToxicRagers.Tests.Carmageddon2.Formats
+{
+    public class TWT
+    {
+        [Fact]
+        public void TWT_Load_IsNotNull()
+        {
+            var twt = TR.TWT.Load(@"Files\Carmageddon2\test.twt");
+
+            Assert.NotNull(twt);
+        }
+
+        [Fact]
+        public void TWT_Extract()
+        {
+            var twt = TR.TWT.Load(@"Files\Carmageddon2\test.twt");
+            var entry = twt.Contents[1];
+
+            byte[] data = twt.Extract(entry);
+
+            Assert.True(entry.Name == "Monkey.txt" && data.Length == 0x35);
+        }
+    }
+}
---
+++ ToxicRagers/Carmageddon2/Formats/c2TWT.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+using ToxicRagers.Helpers;
+
+namespace ToxicRagers.Carmageddon2.Formats
+{
+    public class TWT
+    {
+        public string Name { get; set; }
+
+        public string Location { get; set; }
+
+        public List<TWTEntry> Contents { get; set; } = new List<TWTEntry>();
+
+        int dataStart;
+
+        public static TWT Create(string path)
+        {
+            TWT twt = new TWT
+            {
+                Name = Path.GetFileNameWithoutExtension(path),
+                Location = Path.GetDirectoryName(path)
+            };
+
+            twt.Save();
+
+            return twt;
+        }
+
+        public static TWT Load(string path)
+        {
+            TWT twt = new TWT
+            {
+                Name = Path.GetFileNameWithoutExtension(path),
+                Location = Path.GetDirectoryName(path)
+            };
+
+            using (FileStream fs = new FileStream(path, FileMode.Open))
+            using (BinaryReader br = new BinaryReader(fs, Encoding.Default))
+            {
+                br.ReadInt32();     // length
+
+                int fileCount = br.ReadInt32();
+
+                for (int i = 0; i < fileCount; i++)
+                {
+                    twt.Contents.Add(new TWTEntry
+                    {
+                        Length = br.ReadInt32(),
+                        Name = br.ReadString(0x34)
+                    });
+                }
+
+                twt.dataStart = (int)br.BaseStream.Position;
+            }
+
+            return twt;
+        }
+
+        public void Save()
+        {
+            using (FileStream fs = new FileStream(Path.Combine(Location, $"{Name}.twt"), FileMode.Create))
+            using (BinaryWriter bw = new BinaryWriter(fs))
+            {
+                bw.Write(0);
+                bw.Write(Contents.Count);
+
+                foreach (TWTEntry entry in Contents)
+                {
+                    bw.Write(entry.Length);
+                    bw.WriteString(entry.Name);
+                    bw.Write((byte)0);
+                    for (int i = entry.Name.Length + 1; i < 52; i++) { bw.Write((byte)205); }
+                }
+
+                foreach (TWTEntry entry in Contents)
+                {
+                    bw.Write(File.ReadAllBytes(entry.SourceFile));
+
+                    if (entry.Length % 4 != 0)
+                    {
+                        for (int i = entry.Length % 4; i < 4; i++) { bw.Write((byte)205); }
+                    }
+                }
+
+                bw.BaseStream.Seek(0, SeekOrigin.Begin);
+                bw.Write((int)bw.BaseStream.Length);
+            }
+        }
+
+        public byte[] Extract(TWTEntry entry)
+        {
+            using (FileStream fs = new FileStream(Path.Combine(Location, $"{Name}.twt"), FileMode.Open))
+            using (BinaryReader br = new BinaryReader(fs))
+            {
+                int offset = dataStart;
+
+                for (int i = 0; i < Contents.Count; i++)
+                {
+                    if (Contents[i] == entry)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        offset += entry.Length;
+
+                        if (entry.Length % 4 > 0) { offset += 4 - (entry.Length % 4); }
+                    }
+                }
+
+                br.BaseStream.Seek(offset, SeekOrigin.Begin);
+
+                return br.ReadBytes(entry.Length);
+            }
+        }
+
+        public void Extract(TWTEntry entry, string destination)
+        {
+            using (BinaryWriter bw = new BinaryWriter(new FileStream(Path.Combine(destination, entry.Name), FileMode.Create)))
+            {
+                bw.Write(Extract(entry));
+            }
+        }
+
+        public void Add(string path)
+        {
+            Contents.Add(TWTEntry.FromFile(path));
+
+            Save();
+        }
+    }
+
+    public class TWTEntry
+    {
+        public int Length { get; set; }
+
+        public string Name { get; set; }
+
+        public string SourceFile { get; set; }
+
+        public static TWTEntry FromFile(string path)
+        {
+            return new TWTEntry
+            {
+                Name = Path.GetFileName(path),
+                Length = (int)new FileInfo(path).Length,
+                SourceFile = path
+            };
+        }
+    }
+}
