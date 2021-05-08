29fb7f2a1ef6945553a3a2ca4f4567b47ddf63e1
Maxx Wyndham <maxx@marketingradar.com>
2021-05-08 09:45:15 +0100

    C1:  Changes to DocumentParser

---
+++ ToxicRagers.Tests/Carmageddon/Helpers/DocumentParser.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+using TR = ToxicRagers.Carmageddon.Helpers;
+
+using Xunit;
+
+namespace ToxicRagers.Tests.Carmageddon.Helpers
+{
+    public class DocumentParser
+    {
+        [Fact]
+        public void DocumentParser_New()
+        {
+            var file = new TR.DocumentParser(@"Files\Carmageddon\TestWithCommentsEncoded.txt");
+
+            Assert.NotNull(file);
+        }
+
+        [Fact]
+        public void DocumentParser_Decode()
+        {
+            var file = new TR.DocumentParser(@"Files\Carmageddon\TestWithCommentsEncoded.txt");
+
+            file.ReadLine();
+
+            Assert.True(file.ReadLine() == "Or a llama.");
+        }
+
+        [Fact]
+        public void DocumentParser_Encode()
+        {
+            var file = new TR.DocumentParser(@"Files\Carmageddon\TestWithCommentsEncoded.txt");
+
+            file.Encode();
+
+            Assert.True(file.Data.Length == 0x68 && file.Data[7] == 0x93);
+        }
+    }
+}
--- ToxicRagers/Carmageddon/Helpers/DocumentParser.cs
+++ ToxicRagers/Carmageddon/Helpers/DocumentParser.cs
@@ -9,56 +9,53 @@ namespace ToxicRagers.Carmageddon.Helpers
 {
     public class DocumentParser
     {
+        static readonly byte[] seeds = new byte[] {
+                0x6C, 0x1B, 0x99, 0x5F,
+                0xB9, 0xCD, 0x5F, 0x13,
+                0xCB, 0x04, 0x20, 0x0E,
+                0x5E, 0x1C, 0xA1, 0x0E
+            };
+
+        static readonly byte[] commentSeeds = new byte[] {
+                0x67, 0xA8, 0xD6, 0x26,
+                0xB6, 0xDD, 0x45, 0x1B,
+                0x32, 0x7E, 0x22, 0x13,
+                0x15, 0xC2, 0x94, 0x37
+            };
+
         byte[] data;
         int position;
 
         public bool EOF => position == data.Length;
+
         public bool Fiddled { get; } = false;
+
+        public byte[] Data => data;
 
         public override string ToString()
         {
             return Encoding.ASCII.GetString(data);
         }
 
-        public DocumentParser(string path)
+        public DocumentParser(string path) : this(new BinaryReader(File.OpenRead(path))) { }
+
+        public DocumentParser(BinaryReader br)
         {
-            using (BinaryReader br = new BinaryReader(File.OpenRead(path)))
-            {
-                if (br.PeekChar() == '@')
-                {
-                    List<byte> processed = new List<byte>();
+            data = br.ReadBytes((int)br.BaseStream.Length);
 
-                    while (br.BaseStream.Position != br.BaseStream.Length)
-                    {
-                        byte[] s = iWantToFiddle(readLine(br, 256));
-                        if (s != null)
-                        {
-                            processed.AddRange(s);
-                            processed.Add(13);
-                            processed.Add(10);
-                        }
-                    }
+            Fiddled = Decode();
 
-                    data = new byte[processed.Count];
-                    processed.CopyTo(data);
-
-                    Fiddled = true;
-                }
-                else
-                {
-                    data = br.ReadBytes((int)br.BaseStream.Length);
-                }
-            }
+            position = 0;
         }
 
-        private byte[] readLine(BinaryReader br, int buffSize)
+        private byte[] readLine(int buffSize)
         {
             byte[] b = new byte[buffSize];
             int i = 0;
 
-            while (br.BaseStream.Position != br.BaseStream.Length)
+            while (position != data.Length)
             {
-                byte c = br.ReadByte();
+                byte c = data[position++];
 
                 if (c == '\r') { continue; }
                 if (c == '\n') { break; }
@@ -78,21 +75,6 @@ namespace ToxicRagers.Carmageddon.Helpers
             if (s[0] != '@') { return null; }
 
             byte[] d = new byte[s.Length - 1];
-
-            byte[] seeds = new byte[] {
-                0x6C, 0x1B, 0x99, 0x5F,
-                0xB9, 0xCD, 0x5F, 0x13,
-                0xCB, 0x04, 0x20, 0x0E,
-                0x5E, 0x1C, 0xA1, 0x0E
-            };
-
-            byte[] commentSeeds = new byte[] {
-                0x67, 0xA8, 0xD6, 0x26,
-                0xB6, 0xDD, 0x45, 0x1B,
-                0x32, 0x7E, 0x22, 0x13,
-                0x15, 0xC2, 0x94, 0x37
-            };
-
             byte[] key = seeds;
 
             Array.Copy(s, 1, d, 0, d.Length);
@@ -101,25 +83,100 @@ namespace ToxicRagers.Carmageddon.Helpers
 
             for (int i = 0; i < len; i++)
             {
-                if (i >= 3 && d[i - 1] == '/' && d[i - 2] == '/')
-                {
-                    key = commentSeeds;
-                }
+                if (i >= 3 && d[i - 1] == '/' && d[i - 2] == '/') { key = commentSeeds; }
 
                 if (d[i] == '\t') { d[i] = 159; }
 
                 byte c = (byte)(d[i] - 32);
 
-                if ((c & 0x80) != 0x80)
-                {
-                    d[i] = (byte)((c ^ (key[seed] & 127)) + 32);
-                }
+                if ((c & 0x80) != 0x80) { d[i] = (byte)((c ^ (key[seed] & 127)) + 32); }
+
                 seed = (seed + 7) % 16;
 
                 if (d[i] == 159) { d[i] = 9; }
             }
 
             return d;
+        }
+
+        private byte[] iDontWantToFiddle(byte[] d)
+        {
+            byte[] key = seeds;
+            int len = d.Length;
+            int seed = len % 16;
+            int count = 0;
+
+            for (int i = 0; i < len; i++)
+            {
+                if (count == 2) { key = commentSeeds; }
+
+                if (d[i] == '/') { count++; } else { count = 0; }
+
+                if (d[i] == '\t') { d[i] = 159; }
+
+                byte c = (byte)(d[i] - 32);
+
+                if ((c & 0x80) != 0x80) { d[i] = (byte)((c ^ (key[seed] & 127)) + 32); }
+
+                seed = (seed + 7) % 16;
+
+                if (d[i] == 159) { d[i] = 9; }
+            }
+
+            return d;
+        }
+
+        public bool Encode()
+        {
+            if (data[0] == '@') { return false; }
+
+            List<byte> processed = new List<byte>();
+
+            position = 0;
+
+            while (position != data.Length)
+            {
+                byte[] s = iDontWantToFiddle(readLine(256));
+
+                if (s != null)
+                {
+                    processed.Add((byte)'@');
+                    processed.AddRange(s);
+                    processed.Add(13);
+                    processed.Add(10);
+                }
+            }
+
+            data = new byte[processed.Count];
+            processed.CopyTo(data);
+
+            return true;
+        }
+
+        public bool Decode()
+        {
+            if (data[0] != '@') { return false; }
+
+            List<byte> processed = new List<byte>();
+
+            position = 0;
+
+            while (position != data.Length)
+            {
+                byte[] s = iWantToFiddle(readLine(256));
+
+                if (s != null)
+                {
+                    processed.AddRange(s);
+                    processed.Add(13);
+                    processed.Add(10);
+                }
+            }
+
+            data = new byte[processed.Count];
+            processed.CopyTo(data);
+
+            return true;
         }
 
         public string ReadLine()
