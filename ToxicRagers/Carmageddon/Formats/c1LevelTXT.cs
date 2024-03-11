d7c981b85c1771c92258aff9beb192b4019c73af
Maxx Wyndham <maxx@marketingradar.com>
2024-03-11 22:00:27 +0000

    Carmageddon Level
    With thanks to DevilboxGames

--- ToxicRagers/Carmageddon/Formats/c1LevelTXT.cs
+++ ToxicRagers/Carmageddon/Formats/c1LevelTXT.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ToxicRagers.Carmageddon.Helpers;
+﻿using ToxicRagers.Carmageddon.Helpers;
 using ToxicRagers.Helpers;
 
 namespace ToxicRagers.Carmageddon.Formats
