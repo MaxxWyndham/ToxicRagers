69129ae4028179e47c7e6c2921d77724cdfd6a99
Maxx Wyndham <maxx@marketingradar.com>
2024-03-16 08:42:41 +0000

    Tests
    Added a TGA Load test

---
+++ ToxicRagers.Tests/Core/Formats/cTGA.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing;
+
+using CoreFormats = ToxicRagers.Core.Formats;
+
+using Xunit;
+
+namespace ToxicRagers.Tests.Core.Formats
+{
+    public class TGA
+    {
+        [Fact]
+        public void TGA_FromBitmap()
+        {
+            var tga = CoreFormats.TGA.FromBitmap((Bitmap)Image.FromFile(@"Files\Core\errol.256x256.png"));
+
+            Assert.NotNull(tga);
+        }
+    }
+}
